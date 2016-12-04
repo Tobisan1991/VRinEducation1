6aa11230619a0f5de54b45bd886d1730c5081926
Daniel <d.koeber@online.de>
2016-12-04 20:49:36 +0100

    initial

---
+++ Assets/InteractiveItem2.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Selectable))]
+public class InteractiveItem2 : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public string nextScene;
+    public bool isEntered = false;
+    public Selectable _selectable;
+    public float GazeActivationTime = 3f;
+    public Text winText;
+
+    BaseInputModule input;
+
+    float timeElapsed;
+
+
+    void Start()
+    {
+        input = FindObjectOfType<BaseInputModule>();
+        _selectable = GetComponent<Selectable>();
+     //   winText.text = "";
+    }
+
+    void Update()
+    {
+        if (!_selectable.IsInteractable())
+        {
+            ReticlePointer.Instance.SetFillAmount(0);
+            return;
+        }
+        if (isEntered)
+        {
+            timeElapsed += Time.deltaTime;
+
+            if (timeElapsed >= GazeActivationTime)
+            {
+                timeElapsed = 0;
+
+                ((GazeInputModule)input).HandleTriggerManually();
+
+                isEntered = false;
+            }
+
+            ReticlePointer.Instance.Show();
+
+        }
+        else
+        {
+            timeElapsed = 0;
+        }
+    }
+
+    void OnDisable()
+    {
+        isEntered = false;
+        if (ReticlePointer.Instance != null)
+        {
+        //    winText.text = "";
+            timeElapsed = 0;
+        }
+    }
+
+    #region IPointerEnterHandler implementation
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_selectable.IsInteractable())
+        {
+            Invoke("SetEnteredTrue", 3f); // start timer after 0.3 seconds of gaze. You can call SetEnteredTrue() directly.
+        }
+    }
+
+    void SetEnteredTrue()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+
+    #endregion
+
+    #region IPointerExitHandler implementation
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!_selectable.IsInteractable())
+            return;
+        try
+        {
+            CancelInvoke("SetEnteredTrue");
+            isEntered = false;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+    }
+    #endregion
+
+    #region IPointerClickHandler implementation
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isEntered = false;
+        timeElapsed = 0;
+
+    }
+    #endregion
+
+}
+
---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MoveCamera : MonoBehaviour
+{
+    float timeLeft = 300.0f;
+    //  bool isLooking = false;
+    public float countdown = 50f;
+
+  public void update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            //            Application.LoadLevel("gameOver");
+            SceneManager.LoadScene("szene2");
+        }
+    }
+
+ 
+
+
+ /*   public void TeleportSzene2()
+    {
+
+        if (isLooking)
+        {
+            countdown -= Time.deltaTime;
+        }
+
+        //Teleport if countdown reaches zero
+        if (countdown <= 0)
+            SceneManager.LoadScene("szene2");
+    }
+    public void TeleportSzene1()
+    {
+          countdown -= Time.deltaTime;
+        
+        if (countdown <= 0)
+            SceneManager.LoadScene("szene1");
+    }
+
+   */ public void AbortScene()
+    {
+        countdown = 5;
+    }
+   
+
+}
