760ebd375e6e3c81773f68cd69d3733bc3d95078
Daniel <d.koeber@online.de>
2016-12-05 14:35:32 +0100

    TextPopUp + Default Szenen für Aran + Murat
    Bitte immer die Default Szene als Master start-szene benutzen und diese
    mit save szene as xxx verwenden.

---
+++ Assets/Script/VRTextScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VRTextScript : MonoBehaviour
+{
+    
+    private bool gazedAt;
+    public Text InfoBox;
+    public Text winBox;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        InfoBox.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PointerIn()
+    {
+        gazedAt = true;
+        InfoBox.text = winBox.text;
+
+    }
+    public void PointerOut()
+    {
+        gazedAt = false;
+
+        InfoBox.text = "";
+
+    }
+
+
+       
+}
+
