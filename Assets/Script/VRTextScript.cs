f612207a7c4cfeb10cab264c098575716b71b882
Daniel <d.koeber@online.de>
2017-01-02 17:55:07 +0100

    Daniel skript update  - working again
    working again

--- Assets/Script/VRTextScript.cs
+++ Assets/Script/VRTextScript.cs
@@ -6,31 +6,39 @@ public class VRTextScript : MonoBehaviour
 {
     
 	//Variable for the Text Gui
-    public Text InfoBox;
+    public Text InfoBox1;
+    public Text InfoBox2;
     public Text winBox;
-    public Image panel;
+    public Image panel1;
+    public Image panel2;
   //  public Image BackgroundText;
   //  public Image BackgroundFull;
   //  public Image BackgroundEmpty;
   
     void Start()
     {
-        InfoBox.text = "";
-        panel.gameObject.SetActive(false);
+        InfoBox1.text = "";
+        InfoBox2.text = "";
+        panel1.gameObject.SetActive(false);
+        panel2.gameObject.SetActive(false);
     }
 
     public void PointerIn()
     {
-        InfoBox.text = winBox.text;
-        panel.gameObject.SetActive(true);
+        InfoBox1.text = winBox.text;
+        InfoBox2.text = winBox.text;
+        panel1.gameObject.SetActive(true);
+        panel2.gameObject.SetActive(true);
     }
     //if out show again a empty InfoBox
     public void PointerOut()
     {
-                InfoBox.text = "";
-        panel.gameObject.SetActive(false);
+        InfoBox1.text = "";
+        InfoBox2.text = "";
+        panel1.gameObject.SetActive(false);
+        panel2.gameObject.SetActive(false);
     }
-    
+
 
 }
 
