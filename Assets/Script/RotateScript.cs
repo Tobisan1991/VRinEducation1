fd1cee8e104be6c3f9e5b8f67de478b5d4297313
Daniel <d.koeber@online.de>
2017-01-08 18:39:10 +0100

    Jawoll für Riko :dash:

---
+++ Assets/Script/RotateScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+  public GameObject objekt;
+    
+    public void PointerDrin()
+    {
+        objekt.transform.Rotate(new Vector3(0,Time.deltaTime,0));
+    }
+    
+}
+
