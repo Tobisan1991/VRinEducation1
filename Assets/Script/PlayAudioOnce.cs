5009de1b4dc12143d1dbbd9b8fd89fb76bf948bd
ripane <ripane@hotmail.de>
2017-01-08 15:19:01 +0100

    Herz Animiert, Herz Sound eingefügt onPointerEnter
    moruh

---
+++ Assets/Script/PlayAudioOnce.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioOnce : MonoBehaviour {
+
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PointerIn()
+    {
+        if (!alreadyPlayed)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            alreadyPlayed = true;
+        }
+    }
+}
