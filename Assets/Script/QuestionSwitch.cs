53350f768b4a096bcbb9371b050e9b3d9a8cd179
pedram92 <p.hashemi92@yahoo.de>
2017-01-10 21:07:11 +0100

    Motor Animation

---
+++ Assets/Script/QuestionSwitch.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class QuestionSwitch : MonoBehaviour
+{
+
+    public Text InfoBox;
+    public Text winBox;
+    private object panel;
+
+    public string ersteFrage;
+    public string zweiteFrage;
+    public string dritteFrage;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        InfoBox.text = ersteFrage;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void PointerIn()
+    {
+        if(InfoBox.text.Equals(ersteFrage))
+        {
+            InfoBox.text = zweiteFrage;
+        } else if (InfoBox.text.Equals(zweiteFrage))
+        {
+            InfoBox.text = dritteFrage;
+        }
+        else if (InfoBox.text.Equals(dritteFrage))
+        {
+            InfoBox.text = "Sie sind durch";
+        }
+
+    }
+    public void PointerOut()
+    {
+
+        InfoBox.text = InfoBox.text;
+    }
+
+
+
+}
