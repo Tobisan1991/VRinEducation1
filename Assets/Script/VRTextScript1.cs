fe5af873c3ab2ccc17464ee2003061c2dc0183c1
Daniel <d.koeber@online.de>
2017-01-11 20:21:17 +0100

    änderungen

---
+++ Assets/Script/VRTextScript1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VRTextScript1 : MonoBehaviour
+{
+
+    //Variable for the Text Gui
+    public GameObject Box1;
+    //  public Image BackgroundText;
+    //  public Image BackgroundFull;
+    //  public Image BackgroundEmpty;
+
+    void Start()
+    {
+        
+        Box1.gameObject.SetActive(false);
+    }
+
+    public void PointerIn()
+    {
+        Box1.gameObject.SetActive(true);
+    }
+    //if out show again a empty InfoBox
+    public void PointerOut()
+    {
+
+        Box1.gameObject.SetActive(false);
+    }
+
+
+}
+
