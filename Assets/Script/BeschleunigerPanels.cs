7ece80eeb4e29b261be6d38c15611773f196561b
ripane <ripane@hotmail.de>
2017-01-11 03:43:21 +0100

    TeilchenBeschleunigerSzeneHinzugefügt
    Ich bin der boss Danke

---
+++ Assets/Script/BeschleunigerPanels.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Selectable))]
+
+public class BeschleunigerPanels : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public GameObject atom1;
+    public GameObject atom2;
+ 
+    public bool isEntered = false;
+    public Selectable _selectable;
+    public float GazeActivationTime = 3;
+
+    BaseInputModule input;
+
+    float timeElapsed;
+
+
+    void Start()
+    {
+        input = FindObjectOfType<BaseInputModule>();
+        _selectable = GetComponent<Selectable>();
+        atom1.SetActive(false);
+        atom2.SetActive(false);
+       
+    }
+
+    void Update()
+    {
+        if (!_selectable.IsInteractable())
+        {
+            ReticlePointer.Instance.SetFillAmount(0);
+            return;
+        }
+        if (isEntered)
+        {
+            timeElapsed += Time.deltaTime;
+
+            if (timeElapsed >= GazeActivationTime)
+            {
+                timeElapsed = 0;
+
+                ((GazeInputModule)input).HandleTriggerManually();
+
+                isEntered = false;
+            }
+
+            ReticlePointer.Instance.Show();
+
+        }
+        else
+        {
+            timeElapsed = 0;
+        }
+    }
+
+    void OnDisable()
+    {
+        isEntered = false;
+        if (ReticlePointer.Instance != null)
+        {
+            timeElapsed = 0;
+        }
+    }
+
+    #region IPointerEnterHandler implementation
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_selectable.IsInteractable())
+        {
+            Invoke("SetEnteredTrue", 2f); // start timer after 0.3 seconds of gaze. You can call SetEnteredTrue() directly.
+        }
+    }
+
+    void SetEnteredTrue()
+    {
+        atom1.SetActive(true);
+        atom2.SetActive(true);
+    }
+
+
+    #endregion
+
+    #region IPointerExitHandler implementation
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!_selectable.IsInteractable())
+            return;
+        try
+        {
+            CancelInvoke("SetEnteredTrue");
+            isEntered = false;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError(ex.Message);
+        }
+    }
+    #endregion
+
+    #region IPointerClickHandler implementation
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isEntered = false;
+        timeElapsed = 0;
+
+    }
+    #endregion
+
+}
+
---
+++ Assets/Script/ObjectColliderScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectColliderScript : MonoBehaviour {
+    public GameObject enstehendesObjekt;
+
+	// Use this for initialization
+	void Start () {
+        //enstehendesObjekt.SetActive(false);
+	
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(gameObject.name + "has collided" + col.gameObject.name);
+        if (col.gameObject.name.Equals("Atom1"))
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+        //Destroy (col.gameObject);
+
+    }
+}
