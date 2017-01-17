6c82bd0494b491159c64c751f8f76a2a823b04ad
Daniel <d.koeber@online.de>
2017-01-17 20:34:56 +0100

    murat bitte weitermachen, kann nicht builden wegen movie texture error danke
    http://answers.unity3d.com/questions/377854/why-cannot-build-movie-texture-on-android.html
    oder
    
    http://stackoverflow.com/questions/40091770/the-type-or-namespace-name-movietexture-could-not-be-found-are-you-missing-a-u

--- Assets/Szenen/MoviePlayer.cs
+++ Assets/Szenen/MoviePlayer.cs
@@ -4,22 +4,24 @@ using UnityEngine.UI;
 public class MoviePlayer : MonoBehaviour {
 
     public MovieTexture movie;
-    private AudioSource audio;
+    private new AudioSource audio;
+   
+    
 
-
-	// Use this for initialization
-	void Start () {
+// Use this for initialization
+void Start () {
         GetComponent<RawImage>().texture = movie as MovieTexture;
         audio = GetComponent<AudioSource>();
-        audio.clip = movie.audioClip;
-
+         audio.clip = movie.audioClip;
+        
         movie.Play();
         audio.Play();
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+        
+        // Constructors
+        
+
+
+}
 }
